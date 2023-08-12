47339893fc3aff3f66df9c43ff3645624286082b
Alikarbasi <91276052+Alikarbasi@users.noreply.github.com>
2023-08-11 20:50:07 -0400

    Update global.js

--- config/global.js
+++ config/global.js
@@ -3,7 +3,7 @@ const configuration = {
     'github': {
         'clientId': 'd03b523ea5104c298230',
         'clientSecret': 'dfc633f3c799d65bbaadf7ee4775dedd58001689',
-        'callbackUrl': 'http://task-tracker-application.onrender.com/callback'
+        'callbackUrl': 'https://task-tracker-application.onrender.com/callback'
     }
 };
 module.exports = configuration;
